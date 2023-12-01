adf5d39935bd653bfdfd16f869f8560561a7ba81
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-12-01 17:25:45 +0300

    fix

--- SwiftUI_task_1/ContentView.swift
+++ SwiftUI_task_1/ContentView.swift
@@ -1,23 +1,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rectangleHeight:CGFloat = 50
     var body: some View {
         TabView {
-            VStack(spacing: 0) {
                 ScrollView {
-
-                        ForEach(1...100, id: \.self) { number in
-                            Text("\(number)")
-                                .font(.system(size: 24))
-                                .foregroundStyle(Color.gray)
-                                .frame(maxWidth: .infinity)
-                        }
-                    
+                    ForEach(1...100, id: \.self) { number in
+                        Text("\(number)")
+                            .font(.system(size: 24))
+                            .foregroundStyle(Color.gray)
+                            .frame(maxWidth: .infinity)
+                    }
                 }
-                .scrollClipDisabled()
-                Rectangle() // Видимый Rectangle
-                    .frame(height: 50)
-                    .foregroundColor(Color.red.opacity(0.75))
+                .safeAreaInset(edge: .bottom, spacing: 0) {
+                    Rectangle()
+                        .frame(height: rectangleHeight)
+                        .foregroundColor(Color.red.opacity(0.75))
             }
             .tabItem {
                 Label("First", systemImage: "star.fill")
