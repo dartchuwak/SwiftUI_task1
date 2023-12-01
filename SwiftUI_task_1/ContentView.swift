ed9abbbecb5f2c61c9d23750633d1251b0683707
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-12-01 15:58:14 +0300

    Done

--- SwiftUI_task_1/ContentView.swift
+++ SwiftUI_task_1/ContentView.swift
@@ -1,24 +1,33 @@
-//
-//  ContentView.swift
-//  SwiftUI_task_1
-//
-//  Created by Evgenii Mikhailov on 01.12.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            VStack(spacing: 0) {
+                ScrollView {
+                    LazyVStack {
+                        ForEach(1...100, id: \.self) { number in
+                            Text("\(number)")
+                                .font(.system(size: 24))
+                                .foregroundStyle(Color.gray)
+                                .frame(maxWidth: .infinity)
+                        }
+                    }
+                }
+                .scrollClipDisabled()
+                Rectangle() // Видимый Rectangle
+                    .frame(height: 50)
+                    .foregroundColor(Color.red.opacity(0.75))
+            }
+            .tabItem {
+                Label("First", systemImage: "star.fill")
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
